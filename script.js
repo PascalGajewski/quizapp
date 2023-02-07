55ade96cdfd82ff14fcb2cd233a1fed3fc89e079
PascalGajewski <pascal.gajewski77@gmail.com>
2023-02-07 18:25:54 +0100

    finished responsive

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function generateQuestionCard() {
         </div>
         <div class="d-flex justify-content-between align-items-center w-100 ps-2 pe-2 pb-3">
             <span class="mb-0" id="indicator"> Frage <b>${(currentQuestion + 1)}</b> von <b>${questions[currentCode].length}</b> </span>
-            <button type="button" class="btn btn-primary gap-3 disabled" onclick="nextQuestion(), calculateProgress()" id="next-btn">
+            <button type="button" class="btn btn-primary gap-3 disabled responsive-smaller" onclick="nextQuestion(), calculateProgress()" id="next-btn">
                 NEXT
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
